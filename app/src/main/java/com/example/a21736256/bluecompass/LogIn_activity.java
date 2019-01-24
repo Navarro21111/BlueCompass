254ce31c8f3b649d73477c48f5e20cf7bd397b39
SERGIO NAVARRO MANZANO <21736256@alumnos.uem.es>
2019-01-24 10:45:14 +0100

    Correccion de cosas en el log in

---
+++ app/src/main/java/com/example/a21736256/bluecompass/LogIn_activity.java
@@ -0,0 +1,13 @@
+package com.example.a21736256.bluecompass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogIn_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_activity);
+    }
+}
