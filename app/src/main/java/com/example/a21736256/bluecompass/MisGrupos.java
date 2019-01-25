dbe106eb060f13e76acf92771c126ca60c70dc81
“mi <Juan.garde,jimenez@gmail.com>
2019-01-25 13:25:48 +0100

    He hecho el layout de mis grupos, de buscar grupo y de crear grupo

---
+++ app/src/main/java/com/example/a21736256/bluecompass/MisGrupos.java
@@ -0,0 +1,13 @@
+package com.example.a21736256.bluecompass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MisGrupos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_grupos);
+    }
+}
