1e48f21580874b436d82a3070d846fb90dd1fbd3
“mi <Juan.garde,jimenez@gmail.com>
2019-02-01 11:12:36 +0100

    Ya esta tambien el pojo de grupo

---
+++ app/src/main/java/com/example/a21736256/bluecompass/javabeans/evento.java
@@ -0,0 +1,11 @@
+package com.example.a21736256.bluecompass.javabeans;
+
+import java.util.ArrayList;
+
+public class evento {
+    int fecha;
+    String playa;
+    String nombre;
+    String provincia;
+
+}
