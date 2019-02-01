348d47ed6b27b2b86f56f17f3b317f2c8c163317
“mi <Juan.garde,jimenez@gmail.com>
2019-02-01 12:28:35 +0100

    El insert del firebase de los eventos

--- app/src/main/java/com/example/a21736256/bluecompass/CrearGrupo.java
+++ app/src/main/java/com/example/a21736256/bluecompass/CrearGrupo.java
@@ -2,12 +2,45 @@ package com.example.a21736256.bluecompass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.a21736256.bluecompass.javabeans.evento;
+import com.google.android.gms.maps.MapView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CrearGrupo extends AppCompatActivity {
+    MapView mv;
+    EditText etFecha;
+    EditText etNombre;
+    EditText etMapa;
+    private DatabaseReference dbR;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_grupo);
+
+        mv = findViewById(R.id.mapaGrupo);
+        etFecha=findViewById(R.id.etFecha);
+        etNombre=findViewById(R.id.etNombreGrupo);
+        etMapa=findViewById(R.id.etmapa);
+        dbR = FirebaseDatabase.getInstance().getReference().child("mensaje");
+
+
     }
+    public void insertarEvento(View v){
+            evento ev = new evento(etFecha.getText().toString(),etNombre.getText().toString(),etMapa.getText().toString());
+            String clave = dbR.push().getKey();
+            dbR.child(clave).setValue(ev);
+
+            etFecha.setText("");
+            etMapa.setText("");
+            etNombre.setText("");
+
+
+    }
+
 }
--- app/src/main/java/com/example/a21736256/bluecompass/javabeans/evento.java
+++ app/src/main/java/com/example/a21736256/bluecompass/javabeans/evento.java
@@ -3,9 +3,46 @@ package com.example.a21736256.bluecompass.javabeans;
 import java.util.ArrayList;
 
 public class evento {
-    int fecha;
+    String fecha;
     String playa;
     String nombre;
     String provincia;
 
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getPlaya() {
+        return playa;
+    }
+
+    public void setPlaya(String playa) {
+        this.playa = playa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public evento(String fecha, String playa, String nombre) {
+        this.fecha = fecha;
+        this.playa = playa;
+        this.nombre = nombre;
+    }
 }
